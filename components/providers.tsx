bb0e8ef8b45fcf837effc3df8a832669ccda5a25
Andrew Reinhart <ndrurei@gmail.com>
2025-11-06 21:30:50 +0700

    fix panna provider build error

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,19 +1,40 @@
-'use client';
+"use client";
 
-import { PannaProvider } from 'panna-sdk';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { PannaProvider } from "panna-sdk";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const clientId = process.env.NEXT_PUBLIC_PANNA_CLIENT_ID;
+  const partnerId = process.env.NEXT_PUBLIC_PANNA_PARTNER_ID;
+
+  const hasPanna = !!clientId && !!partnerId;
+
   return (
-    <PannaProvider
-      clientId={process.env.NEXT_PUBLIC_PANNA_CLIENT_ID}
-      partnerId={process.env.NEXT_PUBLIC_PANNA_PARTNER_ID}
-    >
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
-        <Toaster />
-      </ThemeProvider>
-    </PannaProvider>
+    <>
+      {hasPanna ? (
+        <PannaProvider clientId={clientId} partnerId={partnerId}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </PannaProvider>
+      ) : (
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      )}
+    </>
   );
 }
